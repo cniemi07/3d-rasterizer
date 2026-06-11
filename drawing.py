50e0b15b58bab3cd0d84b9948d7fce88a1ed30e2
cniemi07 <cpniemi@mtu.edu>
2026-06-11 18:40:32 -0400

    added rotation

--- drawing.py
+++ drawing.py
@@ -1,11 +1,18 @@
 import pygame
+import math
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 focal_length = 350
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+def rotateY(theta, x, y, z):
+    new_x = x*math.cos(theta) +  z*math.sin(theta)
+    new_z = -x*math.sin(theta) + z*math.cos(theta)
+    new_y = y
 
-def draw_lineH(x0, y0, x1, y1):
+    return (new_x, new_y, new_z)
+
+
+def draw_lineH(screen, x0, y0, x1, y1):
 
     if x0 > x1:
         x0, x1 = x1, x0
@@ -30,7 +37,7 @@ def draw_lineH(x0, y0, x1, y1):
                 p = p + 2*dy - 2*dx
             else:
                 p = p + 2*dy
-def draw_lineV(x0, y0, x1, y1):
+def draw_lineV(screen, x0, y0, x1, y1):
 
     if y0 > y1:
         x0, x1 = x1, x0
@@ -55,26 +62,31 @@ def draw_lineV(x0, y0, x1, y1):
                 p = p + 2*dx - 2*dy
             else:
                 p = p + 2*dx    
-def draw_line(x0, y0, x1, y1):
+def draw_line(screen, x0, y0, x1, y1):
     if abs(x1-x0) > abs(y1-y0):
-        draw_lineH(x0, y0, x1, y1)
-    else: draw_lineV(x0, y0, x1, y1)
+        draw_lineH(screen, x0, y0, x1, y1)
+    else: draw_lineV(screen, x0, y0, x1, y1)
     
-def draw_cube(front, back):
+def draw_cube(screen, front, back):
     for i in range(len(front)):
-        draw_line(*front[i], *back[i])
-    for i in range(len(front)):
-        draw_line(*front[i], *front[(i + 1) % 4])
-    for i in range(len(front)):
-        draw_line(*back[i], *back[(i + 1) % 4])
+        if front[i] is not None and back[i] is not None:
+            draw_line(screen, *front[i], *back[i])
+    for i in range(4):
+        if front[i] is not None and front[(i+1)%4] is not None:
+            draw_line(screen, *front[i], *front[(i+1)%4])
+    for i in range(4):
+        if back[i] is not None and back[(i+1)%4] is not None:
+            draw_line(screen, *back[i], *back[(i+1)%4])
     
 def project(x, y, z):
 
     if z > 0:
         x_screen = (x * focal_length) / z
         y_screen = (y * focal_length) / z
-        
+            
         x_pixel = x_screen + (SCREEN_WIDTH / 2)
         y_pixel = -y_screen + (SCREEN_HEIGHT / 2)
 
         return (int(x_pixel), int(y_pixel))
+    else: 
+        return None
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ pygame.init()
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
-
+theta = 0
 
     
             
@@ -13,26 +13,43 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 front_3d = [(-1, 1, 3), (1, 1, 3), (1, -1, 3), (-1, -1, 3)]
 back_3d  = [(-1, 1, 5), (1, 1, 5), (1, -1, 5), (-1, -1, 5)]
 
-
+front_3d_2 = [(1, 1, 3), (3, 1, 3), (3, -1, 3), (1, -1, 3)]
+back_3d_2  = [(1, 1, 5), (3, 1, 5), (3, -1, 5), (1, -1, 5)]
 
 
 run = True
 while run:
 
     screen.fill((0, 0, 0))
-            
-    front_2d = [dw.project(*p) for p in front_3d]
-    back_2d = [dw.project(*p) for p in back_3d]
-    dw.draw_cube(front_2d, back_2d)
+    
+    
+
+    front_rotated = [dw.rotateY(theta, *p) for p in front_3d]
+    back_rotated = [dw.rotateY(theta, *p) for p in back_3d]
+
+    front_2d = [dw.project(*p) for p in front_rotated]
+    back_2d = [dw.project(*p) for p in back_rotated]
+    front_2d_2 = [dw.project(*p) for p in front_3d_2]
+    back_2d_2 = [dw.project(*p) for p in back_3d_2]
+    dw.draw_cube(screen, front_2d, back_2d)
+    dw.draw_cube(screen,front_2d_2, back_2d_2)
+
+    keys = pygame.key.get_pressed() 
 
     for event in pygame.event.get():
-        keys = pygame.key.get_pressed() 
+       
         if event.type == pygame.QUIT:
           run = False
         if keys[pygame.K_UP] or keys[pygame.K_w]:
            dw.focal_length += 10
         if keys[pygame.K_DOWN] or keys[pygame.K_s]:
            dw.focal_length -= 10
+        if keys[pygame.K_LEFT] or keys[pygame.K_a]:
+           theta += .05
+        if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+           theta -= .05
+        
+        
     
 
 
