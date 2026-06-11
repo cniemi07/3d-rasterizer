a908a66482cc09a28327d1fa4308c754bda77066
cniemi07 <cpniemi@mtu.edu>
2026-06-11 18:08:32 -0400

    Added 3d - 2d pixel projection and zoom ability by increasing/decreasing focal_length

---
+++ drawing.py
@@ -0,0 +1,80 @@
+import pygame
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+focal_length = 350
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+def draw_lineH(x0, y0, x1, y1):
+
+    if x0 > x1:
+        x0, x1 = x1, x0
+        y0, y1 = y1, y0
+
+    dx = x1 - x0
+    dy = y1 - y0
+
+    dir = -1 if dy < 0 else 1
+    dy *= dir
+
+    if dx != 0:
+
+        y = y0
+        p = 2*dy - dx  # initial decision value
+
+        for i in range(dx + 1):
+            pygame.Surface.set_at(screen, (x0 + i, y), (255, 0, 0))
+
+            if p >= 0:
+                y += dir
+                p = p + 2*dy - 2*dx
+            else:
+                p = p + 2*dy
+def draw_lineV(x0, y0, x1, y1):
+
+    if y0 > y1:
+        x0, x1 = x1, x0
+        y0, y1 = y1, y0
+
+    dx = x1 - x0
+    dy = y1 - y0
+
+    dir = -1 if dx < 0 else 1
+    dx *= dir
+
+    if dy != 0:
+
+        x = x0
+        p = 2*dx - dy  # initial decision value
+
+        for i in range(dy + 1):
+            pygame.Surface.set_at(screen, (x, y0 + i), (255, 0, 0))
+
+            if p >= 0:
+                x += dir
+                p = p + 2*dx - 2*dy
+            else:
+                p = p + 2*dx    
+def draw_line(x0, y0, x1, y1):
+    if abs(x1-x0) > abs(y1-y0):
+        draw_lineH(x0, y0, x1, y1)
+    else: draw_lineV(x0, y0, x1, y1)
+    
+def draw_cube(front, back):
+    for i in range(len(front)):
+        draw_line(*front[i], *back[i])
+    for i in range(len(front)):
+        draw_line(*front[i], *front[(i + 1) % 4])
+    for i in range(len(front)):
+        draw_line(*back[i], *back[(i + 1) % 4])
+    
+def project(x, y, z):
+
+    if z > 0:
+        x_screen = (x * focal_length) / z
+        y_screen = (y * focal_length) / z
+        
+        x_pixel = x_screen + (SCREEN_WIDTH / 2)
+        y_pixel = -y_screen + (SCREEN_HEIGHT / 2)
+
+        return (int(x_pixel), int(y_pixel))
--- main.py
+++ main.py
@@ -1,90 +1,40 @@
 import pygame
-
+import drawing as dw
 pygame.init()
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
-def draw_lineH(x0, y0, x1, y1):
 
-    if x0 > x1:
-        x0, x1 = x1, x0
-        y0, y1 = y1, y0
-
-    dx = x1 - x0
-    dy = y1 - y0
-
-    dir = -1 if dy < 0 else 1
-    dy *= dir
-
-    if dx != 0:
-
-        y = y0
-        p = 2*dy - dx  # initial decision value
-
-        for i in range(dx + 1):
-            pygame.Surface.set_at(screen, (x0 + i, y), (255, 0, 0))
-
-            if p >= 0:
-                y += dir
-                p = p + 2*dy - 2*dx
-            else:
-                p = p + 2*dy
-def draw_lineV(x0, y0, x1, y1):
-
-    if y0 > y1:
-        x0, x1 = x1, x0
-        y0, y1 = y1, y0
-
-    dx = x1 - x0
-    dy = y1 - y0
-
-    dir = -1 if dx < 0 else 1
-    dx *= dir
-
-    if dy != 0:
-
-        x = x0
-        p = 2*dx - dy  # initial decision value
-
-        for i in range(dy + 1):
-            pygame.Surface.set_at(screen, (x, y0 + i), (255, 0, 0))
-
-            if p >= 0:
-                x += dir
-                p = p + 2*dx - 2*dy
-            else:
-                p = p + 2*dx    
-def draw_line(x0, y0, x1, y1):
-    if abs(x1-x0) > abs(y1-y0):
-        draw_lineH(x0, y0, x1, y1)
-    else: draw_lineV(x0, y0, x1, y1)
-def draw_cube(front, back):
-    for i in range(len(front)):
-        draw_line(*front[i], *back[i])
-    for i in range(len(front)):
-        draw_line(*front[i], *front[(i + 1) % 4])
-    for i in range(len(front)):
-        draw_line(*back[i], *back[(i + 1) % 4])
-    
     
             
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-front = [(300, 200), (500, 200), (500, 400), (300, 400)]
-back =  [(380, 120), (580, 120), (580, 320), (380, 320)]
-draw_cube(front, back)
+
+front_3d = [(-1, 1, 3), (1, 1, 3), (1, -1, 3), (-1, -1, 3)]
+back_3d  = [(-1, 1, 5), (1, 1, 5), (1, -1, 5), (-1, -1, 5)]
+
+
+
 
 run = True
 while run:
 
- 
+    screen.fill((0, 0, 0))
             
-
-    
+    front_2d = [dw.project(*p) for p in front_3d]
+    back_2d = [dw.project(*p) for p in back_3d]
+    dw.draw_cube(front_2d, back_2d)
 
     for event in pygame.event.get():
+        keys = pygame.key.get_pressed() 
         if event.type == pygame.QUIT:
           run = False
+        if keys[pygame.K_UP] or keys[pygame.K_w]:
+           dw.focal_length += 10
+        if keys[pygame.K_DOWN] or keys[pygame.K_s]:
+           dw.focal_length -= 10
+    
+
 
     pygame.display.update()
 
